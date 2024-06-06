7af81803a6c39a24f27dac48ecf8923e2aada17d
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-06-06 08:56:55 +0330

    update packages

--- src/Core/Domic.Domain/Commons/Contracts/Interfaces/IQueryUnitOfWork.cs
+++ src/Core/Domic.Domain/Commons/Contracts/Interfaces/IQueryUnitOfWork.cs
@@ -2,4 +2,4 @@ using Domic.Core.Domain.Contracts.Interfaces;
 
 namespace Domic.Domain.Commons.Contracts.Interfaces;
 
-public interface IQueryUnitOfWork : IUnitOfWork;
+public interface IQueryUnitOfWork : ICoreQueryUnitOfWork;
